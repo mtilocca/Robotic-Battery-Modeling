d437a93a4072d4af86e3faed3c7af436d6fc5218
mariotilocca98 <mario.tilocca@gmail.com>
2022-06-21 15:41:58 +0300

    SoC map through empiric data done

--- EScooter_Bat/Data_fit.py
+++ EScooter_Bat/Data_fit.py
@@ -1,3 +1,4 @@
+from turtle import color
 import sklearn
 import pandas as pd 
 import matplotlib.pyplot as plt 
@@ -19,24 +20,71 @@ class SoC_pred:
         self.data_OutBoost = pd.read_csv(r"/Users/mariotilocca/Desktop/Master/Trento/courses/BatteryPJ/Robotic-Battery-Modeling/EScooter_Bat/OutputBoost.txt")
         self.data_InBoost = pd.read_csv(r"/Users/mariotilocca/Desktop/Master/Trento/courses/BatteryPJ/Robotic-Battery-Modeling/EScooter_Bat/InputBoost.txt")
         
-        plt.plot(self.data_InBoost)
-        plt.plot(self.data_OutBoost, color = 'red')
-        plt.show()
-        plt.close()
+        #plt.plot(self.data_InBoost)
+       # plt.plot(self.data_OutBoost, color = 'red')
+       # plt.show()
+       # plt.close()
         self.index_boost = np.arange(len(self.data_InBoost))
         
+        self.index_boost = pd.DataFrame(self.index_boost)
+        
 
-        slope, intercept, r, p, std_err = stats.linregress(self.index_boost, self.data_InBoost.data)
+        #def myfunc(x):
+            #return slope * x + intercept
 
-        def myfunc(x):
-            return slope * x + intercept
+        #mymodel = list(map(myfunc, self.index_boost))
 
-        mymodel = list(map(myfunc, self.index_boost))
-
-        plt.scatter(self.index_boost, self.data_OutBoost)
-        plt.plot(self.index_boost, mymodel)
+        #plt.scatter(self.data_InBoost, self.data_OutBoost)  # diff size 
+        #plt.plot(self.index_boost, mymodel)
+        #plt.show()
+        
+    def normalize_data(self):
+            
+        self.data_OutBoost = self.data_OutBoost.loc[0:len(self.data_InBoost)-1]
+        self.data_buckIn = self.data_buckIn.loc[0:len(self.data_OutBuck)-1]
+            
+            
+    def plot_raw(self):
+        
+        plt.plot(self.data_InBoost, color='green')
+        plt.xlabel('Samples  number [n]')
+        plt.ylabel('Input Voltage [V]')
+        plt.title('Boost discharge cycle')
+        plt.grid(True)
         plt.show()
+        
+ 
 
+            
+    def fit_data(self, plot = False):
+
+        
+        
+        minSL = self.data_InBoost.min()
+        maxSL = self.data_InBoost.max()
+        x = self.data_InBoost
+        f = lambda x: ((x - minSL)*(100-(0)))/(maxSL - minSL) + 0 
+        self.Soc_emp  = x.applymap(f)
+        
+        
+        if plot: 
+
+            
+            plt.plot(self.Soc_emp, self.data_InBoost, color='b')
+            plt.ylabel('Capacitors Voltage [V]')
+            plt.xlabel('SoC [%]')
+            plt.title('SoC Map')
+            plt.grid(True)
+            plt.show()
 
 if __name__ == "__main__":
     soc = SoC_pred()
+    
+    soc.normalize_data()
+    
+    
+    soc.fit_data(False)
+
+   # soc.Soc_emp.to_csv('SoC_map.csv', index=False, header=False)
+
+    soc.plot_raw()
