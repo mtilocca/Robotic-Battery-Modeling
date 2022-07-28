7f8bd80e37f06094d2c6f712625258605db4fc8e
mariotilocca98 <mario.tilocca@gmail.com>
2022-07-28 18:09:36 +0200

    Escooter battery pack Simulink v.2

---
+++ Simulink/cellConstants.m
@@ -0,0 +1,11 @@
+m = 0.176; % mass of the cell [Kg]
+Se = 11; % specific energy [Wh/kg]
+Pe = 9400; % power density [W/kg]  
+Vmax = 3.8 ; % max voltage 
+Vmin = 2.2; % min voltage 
+r = 0.00139; % internal res [ Ohm] 
+C = 1500; % capacitance [F]
+
+W = 0.1202 ; % width of the cell [m]
+H = 0.0718; % height of the cell 
+T = 0.0128;  % length of the cell 
